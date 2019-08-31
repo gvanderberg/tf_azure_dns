8adf41b7c9bacd47d5eb3940674e1a240b74d234
Gareth van der Berg <gareth.shaun@gmail.com>
2019-08-31 13:51:58 +0200

    initial resource group

--- src/output.tf
+++ src/output.tf
@@ -6,10 +6,10 @@ output "resource_group_location" {
   value = azurerm_resource_group.this.location
 }
 
-output "dns_zone_id" {
-  value = azurerm_dns_zone.this.id
-}
+# output "dns_zone_id" {
+#   value = azurerm_dns_zone.this.id
+# }
 
-output "dns_zone_name_servers" {
-  value = azurerm_dns_zone.this.name_servers
-}
+# output "dns_zone_name_servers" {
+#   value = azurerm_dns_zone.this.name_servers
+# }
